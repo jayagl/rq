56bb0bb7c795306859046533cf035b5a900c4bf7
jayagl <jagrawal@isw.net.au>
2016-01-12 17:44:53 +1100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// Basic Gulp File
+//
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefix = require('gulp-autoprefixer'),
+    notify = require("gulp-notify"),
+    bower = require('gulp-bower'),
+    livereload = require('gulp-livereload');
+
+var config = {
+    sassPath: './sass',
+    bowerDir: './lib',
+    imgPath: './images',
+    indexPath:'./index.html',
+    jsPath:'./js'
+};
+
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(sass({
+            style: 'compressed',
+            loadPath: [
+                config.sassPath,
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets'
+            ]
+        })
+            .on("error", notify.onError(function (error) {
+                return "Error: " + error.message;
+            })))
+        .pipe(autoprefix('last 2 version'))
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+  gulp.watch(config.imgPath + '/**/*').on('change', livereload.changed);
+  gulp.watch(config.indexPath).on('change', livereload.changed);
+  gulp.watch(config.jsPath + '/**/*.js').on('change', livereload.changed);
+});
+
+gulp.task('default', ['css', 'watch']);
---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i=0; i<slides.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i]
+    })
+    .setPin(slides[i])
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  }
+
+  var scene = new ScrollMagic.Scene({triggerElement: "header"})
+								.setPin("header")
+								.addIndicators({name: "header"}) // add indicators (requires plugin)
+								.addTo(controller);
+});
